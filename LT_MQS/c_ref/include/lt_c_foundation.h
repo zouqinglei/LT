8e9f4b84d56a14d38a061815c3c44e4fe83084c1
zouqinglei <zouqinglei@163.com>
2016-08-15 11:18:52 +0800

    add a LT_MQS directory

---
+++ LT_MQS/c_ref/include/lt_c_foundation.h
@@ -0,0 +1,38 @@
+/*
+    lt_c_foundation.h
+    all c headers collection
+
+    write by zouql  20140724
+    zouqinglei@163.com 
+    All right reserved.
+*/
+
+#ifndef LT_C_FOUNDATION_H__
+#define LT_C_FOUNDATION_H__
+
+#include "lt_const.h"
+#include "lt_commsocket.h"
+#include "lt_commcircbuf.h"
+#include "lt_pthread.h"
+#include "lt_list_s.h"
+#include "lt_singlethread.h"
+#include "lt_threadpool.h"
+#include "lt_utils.h"
+#include "lt_userlog.h"
+#include "myiterator.h"
+#include "mylinkedlist_cache.h"
+#include "lt_clientsocket.h"
+#include "lt_serversocket.h"
+#include "lt_record.h"
+#include "lt_json_config.h"
+
+#include "json/json_short.h"
+#include "sqlite3.h"
+#include "zlib/zlib.h"
+#include "zlib/zconf.h"
+
+#ifdef WIN32
+#pragma warning(disable : 4996)
+#endif
+
+#endif /*LT_C_FOUNDATION_H__*/
